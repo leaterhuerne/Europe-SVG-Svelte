976b4f15e2bf253bf9a6e4fb068e1ada6f750361
leaterhuerne <102804046+leaterhuerne@users.noreply.github.com>
2023-04-07 17:37:23 +0200

    Add files via upload
    code

---
+++ Europe.ts
@@ -0,0 +1,42 @@
+export class Europe {
+    /**
+     * This class describes the continent Europe by having a field
+     * with a color for each country.
+     * Default color is dark blue saved in value.
+     */
+    DE = {key: "DE", id: "germany", color: "#00008b", de: "Deutschland", en: "Germany"};
+    UK = {key: "UK", id: "unitedKingdom", color: "#00008b", de: "Großbritannien", en: "United Kingdom"};
+    FR = {key: "FR", id: "france", color: "#00008b", de: "Frankreich", en: "France"};
+    IT = {key: "IT", id: "italy", color: "#00008b", de: "Italien", en: "Italy"};
+    ES = {key: "ES", id: "spain", color: "#00008b", de: "Spanien", en: "Spain"};
+    UA = {key: "UA", id: "ukraine", color: "#00008b", de: "Ukraine", en: "Ukraine"};
+    PL = {key: "PL", id: "poland", color: "#00008b", de: "Polen", en: "Poland"};
+    RO = {key: "RO", id: "romania", color: "#00008b", de: "Rumänien", en: "Romania"};
+    NL = {key: "NL", id: "netherlands", color: "#00008b", de: "Niederlande", en: "Netherlands"};
+    BE = {key: "BE", id: "belgium", color: "#00008b", de: "Belgien", en: "Belgium"};
+    CZ = {key: "CZ", id: "czechia", color: "#00008b", de: "Tschechien", en: "Czechia"};
+    EL = {key: "EL", id: "greece", color: "#00008b", de: "Griechenland", en: "Greece"};
+    PT = {key: "PT", id: "portugal", color: "#00008b", de: "Portugal", en: "Portugal"};
+    SE = {key: "SE", id: "sweden", color: "#00008b", de: "Schweden", en: "Sweden"};
+    HU = {key: "HU", id: "hungary", color: "#00008b", de: "Ungarn", en: "Hungary"};
+    BY = {key: "BY", id: "belarus", color: "#00008b", de: "Belarus", en: "Belarus"};
+    AT = {key: "AT", id: "austria", color: "#00008b", de: "Österreich", en: "Austria"};
+    CH = {key: "CH", id: "switzerland", color: "#00008b", de: "Schweiz", en: "Switzerland"};
+    BG = {key: "BG", id: "bulgaria", color: "#00008b", de: "Bulgarien", en: "Bulgaria"};
+    DK = {key: "DK", id: "denmark", color: "#00008b", de: "Dänemark", en: "Denmark"};
+    FI = {key: "FI", id: "finland", color: "#00008b", de: "Finland", en: "Finland"};
+    SK = {key: "SK", id: "slovakia", color: "#00008b", de: "Slowakei", en: "Slovakia"};
+    NO = {key: "NO", id: "norway", color: "#00008b", de: "Norwegen", en: "Norway"};
+    IE = {key: "IE", id: "ireland", color: "#00008b", de: "Irland", en: "Ireland"};
+    HR = {key: "HR", id: "croatia_serbia_montenegro", color: "#00008b", de: "Kroatien, Serbien und Montenegro", en: "Croatia, Serbia and Montenegro"};
+    MD = {key: "MD", id: "moldova", color: "#00008b", de: "Moldawien", en: "Moldova"};
+    BA = {key: "BA", id: "bosniaAndHerzegovina", color: "#00008b", de: "Bosnien und Herzegowina", en: "Bosnia and Herzegovina"};
+    AL = {key: "AL", id: "albania", color: "#00008b", de: "Albanien", en: "Albania"};
+    LT = {key: "LT", id: "lithuania", color: "#00008b", de: "Litauen", en: "Lithuania"};
+    MK = {key: "MK", id: "northMacedonia", color: "#00008b", de: "Nordmazedonien", en: "North Macedonia"};
+    SI = {key: "SI", id: "slovenia", color: "#00008b", de: "Slowenien", en: "Slovenia"};
+    LV = {key: "LV", id: "latvia", color: "#00008b", de: "Lettland", en: "Latvia"};
+    EE = {key: "EE", id: "estonia", color: "#00008b", de: "Estland", en: "Estonia"};
+    LU = {key: "LU", id: "luxembourg", color: "#00008b", de: "Luxemburg", en: "Luxembourg"};
+    TR = {key: "TR", id: "turkey", color: "#00008b", de: "Türkei", en: "Turkey"};
+}
